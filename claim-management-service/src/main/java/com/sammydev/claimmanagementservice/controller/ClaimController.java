ca07c2a05b1cebdc1d780e9ee8bc94301cc1f9e2
sammy obanyi ocharo <obanyisammy22@gmail.com>
2024-06-27 23:32:47 +0300

    create claim

--- claim-management-service/src/main/java/com/sammydev/claimmanagementservice/controller/ClaimController.java
+++ claim-management-service/src/main/java/com/sammydev/claimmanagementservice/controller/ClaimController.java
@@ -2,9 +2,11 @@ package com.sammydev.claimmanagementservice.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/claims/")
 public class ClaimController {
 
     @PostMapping("create-claim/")
